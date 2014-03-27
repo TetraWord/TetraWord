bdca486c218cbc2e8effe5f360e734ca9a90ecff
Lionel <lionel.mullot@gmail.com>
2014-03-27 12:34:33 +0100

    Séparation des gestionnaires d'évènement pour avoir un code plus light dans le contextManager. Création du dictionnaire : - Un constructeur qui initialise un hashset en lisant le fichier media dictionnary.txt - Une méthode boolean included(String s) pour savoir si le mot s est dans le dictionnaire

---
+++ src/ContextManager/GameEventListener.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ContextManager;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class GameEventListener implements ActionListener{
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+  }
+  
+}
