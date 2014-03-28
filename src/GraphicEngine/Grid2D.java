d51e50ca3e412a51ff5dbb592e364a73ae2eef00
Lucie2lr <lucie2lr@gmail.com>
2014-03-28 15:06:50 +0100

    Graphic du GraphicEngine renommer en Graphic2D

---
+++ src/GraphicEngine/Grid2D.java
@@ -0,0 +1,32 @@
+
+package GraphicEngine;
+
+import static GraphicEngine.GraphicEngine.WINDOW_HEIGHT;
+import static GraphicEngine.GraphicEngine.WINDOW_WIDTH;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+class Grid extends JPanel {
+  
+  public Grid(){}
+  
+  @Override
+  public void paintComponent(Graphics g){
+    int step = 30;
+    int marge = 30;
+    int height = WINDOW_HEIGHT - marge;
+    int width = WINDOW_WIDTH;
+    int offsetWidth = width - 11*step;
+    int offsetHeight = height - 21*step;
+    
+    for( int i = marge; i <= width - offsetWidth; i = i + step){
+      for( int j = offsetHeight; j <= height; j = j + step){
+        //horizontal lines
+        g.drawLine(marge, j, width - offsetWidth, j);
+        //vertical lines
+        g.drawLine(i, offsetHeight, i, height - marge);
+      }
+    }
+  }
+  
+}
