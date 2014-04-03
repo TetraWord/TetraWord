e824372a51bbf9a943a08fe9a5cbc3c4cb2cc014
MarineDos <marinedos@gmail.com>
2014-04-03 17:57:45 +0200

    Sauvegarde des préférences de design

--- src/GraphicEngine/DesignMenu2D.java
+++ src/GraphicEngine/DesignMenu2D.java
@@ -5,23 +5,28 @@ import static GraphicEngine.GraphicEngine.WINDOW_WIDTH;
 import static GraphicEngine.GraphicEngine.WINDOW_HEIGHT;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Enumeration;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.swing.AbstractButton;
 import javax.swing.ButtonGroup;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 public class DesignMenu2D extends Menu2D {
 
 	DesignMenu dm;
 	String apercuBackground;
 	String[] bricks;
-	JPanel pannel = new JPanel();
 	ButtonGroup group = new ButtonGroup();
+	ComboBox combo;
 
 	public DesignMenu2D() {
 		super();
@@ -30,8 +35,7 @@ public class DesignMenu2D extends Menu2D {
 		int sx = 150, sy = 60;
 		int x = WINDOW_WIDTH / 2 - sx;
 		int y = 50;
-		int step_y = y + sy;
-		int step_x = y + sx;
+		int step_x = 200;
 
 		dm = new DesignMenu();
 
@@ -40,10 +44,67 @@ public class DesignMenu2D extends Menu2D {
 		this.add(label);
 
 		x = x + step_x;
-		ComboBox combo = new ComboBox("Choose theme", dm.getNames(), x, y, sx, sy);
+		combo = new ComboBox("Choose theme", dm.getNames(), x, y, sx, sy);
 		this.add(combo);
 
-		loadApercus((String)combo.getSelectedItem());
+		x = WINDOW_WIDTH / 2 - sx / 2;
+		y = 650;
+		int step_y = 100;
+		setButton2D(2);
+		Button2D b = new Button2D("Save design", x, y, sx, sy);
+		b.removeListener();
+		b.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				RadioButton2D brick = getSelectedButtonText(group);
+				String imgBrick = brick.getImage();
+				
+				Properties prop = new Properties();
+				OutputStream output = null;
+				try {
+
+					output = new FileOutputStream("conf/myConf.properties");
+
+					// set the properties value
+					prop.setProperty("background", apercuBackground);
+					prop.setProperty("brick", imgBrick);
+
+					// save properties to project root folder
+					prop.store(output, null);
+
+				} catch (IOException io) {
+					io.printStackTrace();
+				} finally {
+					if (output != null) {
+						try {
+							output.close();
+						} catch (IOException eio) {
+							eio.printStackTrace();
+						}
+					}
+
+				}
+			}			
+
+			public RadioButton2D getSelectedButtonText(ButtonGroup buttonGroup) {
+				for (Enumeration<AbstractButton> buttons = buttonGroup.getElements(); buttons.hasMoreElements();) {
+					AbstractButton button = buttons.nextElement();
+
+					if (button.isSelected()) {
+						return (RadioButton2D) button;
+					}
+				}
+
+				return null;
+			}
+		});
+		
+		this.add(b);
+		y = y + step_y;
+		this.add(new Button2D("Previous", x, y, sx, sy));
+
+		loadApercus((String) combo.getSelectedItem());
 	}
 
 	@Override
@@ -88,12 +149,12 @@ public class DesignMenu2D extends Menu2D {
 	}
 
 	public void loadApercus(String item) {
-		
+
 		int x = WINDOW_WIDTH / 4;
 		int y = WINDOW_HEIGHT / 2 + 100;
 		int step = WINDOW_WIDTH / 7;
 		bricks = null;
-		
+
 		/*Get chosen theme*/
 		String[] themes = dm.getNames();
 		for (int i = 0; i < themes.length; ++i) {
@@ -103,7 +164,7 @@ public class DesignMenu2D extends Menu2D {
 				bricks = dm.getBricks(i);
 			}
 		}
-		
+
 		while (group.getElements().hasMoreElements()) {
 			/*Empty button from group and Jpanel */
 			AbstractButton b = group.getElements().nextElement();
@@ -112,13 +173,13 @@ public class DesignMenu2D extends Menu2D {
 		}
 		/*Fill button to group and Jpanel with new bricks*/
 		for (int i = 0; i < bricks.length; ++i) {
-			RadioButton2D radio = new RadioButton2D("Brick");
+			RadioButton2D radio = new RadioButton2D("Brick", bricks[i]);
 			radio.setText("");
 			radio.setBounds(x + 8 + i * step, y + 40, 20, 20);
 			if (i == 0) {
 				radio.setSelected(true);
 			}
-			
+
 			group.add(radio);
 			this.add(radio);
 		}
--- src/GraphicEngine/RadioButton2D.java
+++ src/GraphicEngine/RadioButton2D.java
@@ -1,20 +1,25 @@
 
 package GraphicEngine;
 
-import javax.swing.Icon;
 import javax.swing.JRadioButton;
 
 public class RadioButton2D extends JRadioButton{
 	
 	private final String name;
+	private final String img;
 	
-	public RadioButton2D(String name){
+	public RadioButton2D(String name, String img){
 		super(name);
 		this.name = name;
+		this.img = img;
 	}
   
   @Override
   public String getName(){
     return name;
+  }
+	
+  public String getImage(){
+    return img;
   }
 }
