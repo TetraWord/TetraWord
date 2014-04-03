8090b78f6cc07a23f8ed351ab96d4b544b553e43
MarineDos <marinedos@gmail.com>
2014-04-03 18:02:00 +0200

    Merge Brick.java

--- src/GameEngine/Brick.java
+++ src/GameEngine/Brick.java
@@ -2,20 +2,9 @@
 package GameEngine;
 
 public class Brick {
-	private char letter;
-  private int number;
-
-	public Brick(char letter, int number) {
+	private final char letter;
+	
+	public Brick(char letter) {
 		this.letter = letter;
-    this.number = number;
 	}
-  
-  public Brick(Brick b){
-    this(b.letter, b.number);
-  }
-  
-  public int getNb(){
-    return number;
-  }
-  
 }
