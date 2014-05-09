818d55407751dd7ac148f1e2fef4ad11c00ccde2
MarineDos <marinedos@gmail.com>
2014-05-09 15:30:08 +0200

    Javadoc Observer & Observable

--- src/Pattern/Observable.java
+++ src/Pattern/Observable.java
@@ -1,10 +1,24 @@
 package Pattern;
 
+/**
+ * <b>Interface for Observable objects</b>
+ */
 public interface Observable {
 
+	/**
+	 * Add an observer to the set of observers
+	 * @param obs The new Observer
+	 */
   public void addObserver(Observer obs);
 
+	/**
+	 * Update all Observer
+	 * @param args Arguments 
+	 */
   public void updateObserver(Object args);
 
+	/**
+	 * Delete all Observer 
+	 */
   public void delAllObserver();
 }
--- src/Pattern/Observer.java
+++ src/Pattern/Observer.java
@@ -1,6 +1,15 @@
 package Pattern;
 
+/**
+ * <b>A class can implement the Observer interface when it wants to be informed of changes in observable objects.</b>
+ * 
+ */
 public interface Observer {
 
+	/**
+	 * This method is called whenever the observed object is changed.
+	 * @param o The observable object
+	 * @param args Arguments passed 
+	 */
   public void update(Observable o, Object args);
 }
