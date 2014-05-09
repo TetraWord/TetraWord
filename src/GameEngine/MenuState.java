bbb1ccbc597ed6c73d86f5feb7a199b12dd36ffe
MarineDos <marinedos@gmail.com>
2014-05-09 14:51:02 +0200

    Javadoc MenuState

--- src/GameEngine/MenuState.java
+++ src/GameEngine/MenuState.java
@@ -1,21 +1,57 @@
 package GameEngine;
 
+/**
+ * Enumeration of state for the menu
+ * 
+ */
 public enum MenuState {
 
+	/**
+	 * Main menu
+	 */
   MAIN("Main Menu"),
+	/**
+	 * Game menu
+	 */
   GAME("Game Menu"),
+	/**
+	 * Options menu
+	 */
   OPTION("Option Menu"),
+	/**
+	 * Design preferences menu
+	 */
   DESIGN("Design Menu"),
+	/**
+	 * Difficulty preferences menu
+	 */
   DIFFICULTY("Difficulty Menu"),
-  MULTI("MultiPlayers Menu"),
+	/**
+	 * Multiplayer menu
+	 */
+	MULTI("MultiPlayers Menu"),
+	/**
+	 * Menu before starting game
+	 */
   BEGIN("Begin Menu");
 
+	/**
+	 * Name of the enumeration
+	 */
   private String name = "";
 
+	/**
+	 * MenuState constructor
+	 * @param name Name of the MenuState
+	 */
   MenuState(String name) {
     this.name = name;
   }
 
+	/**
+	 * Get the name of the MenuState
+	 * @return Name of the MenuState
+	 */
   public String getStateName() {
     return name;
   }
