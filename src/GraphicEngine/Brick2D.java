e02c54e4fd7a06ce9e08989dd6241ba2539aef66
MarineDos <marinedos@gmail.com>
2014-05-09 13:02:59 +0200

    Javadoc Brick2D

--- src/GraphicEngine/Brick2D.java
+++ src/GraphicEngine/Brick2D.java
@@ -16,21 +16,61 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
+/**
+ * <b> Brick2D is the graphic representation of a brick </b>
+ * <p>
+ * The Brick2D contains :
+ * <ul>
+ * <li> A Brick model</li>
+ * <li> The pathname of the image file of the brick </li>
+ * </ul>
+ * 
+ * </p>
+ *
+ * @see Brick
+ */
 public class Brick2D {
-
+	
+	/**
+	 * The logical represention of a brick
+	 * 
+	 * @see Brick
+	 */
 	private final Brick model;
+	/**
+	 * The pathname of the image file of all bricks
+	 */
 	private static String brickImg;
-
+	
+	/**
+	 * Brick2D constructor
+	 * 
+	 * @param model Logical represention of the brick
+	 */
 	public Brick2D(Brick model) {
 		this.model = model;
 	}
 
+	/**
+	 * Initialize the image file pathname
+	 * @param brickImg Image file pathname
+	 */
 	public static void setBrickImage(String brickImg) {
 		Brick2D.brickImg = brickImg;
 	}
 
+	/**
+	 * Draw the Brick representation
+	 * 
+	 * @param g Graphics to draw on
+	 * @param top Top position
+	 * @param left Left position
+	 * @param ratio Ratio of drawing
+	 * @param shadow Is a shadow ?
+	 */
 	public void draw(Graphics g, int top, int left, double ratio, boolean shadow) {
 
+		/* Set the color to use for the brick */
 		Color c;
 		if (model.isClicked()) {
       if(model.isDoubleClicked()){
@@ -41,7 +81,9 @@ public class Brick2D {
 		} else {
 			c = model.getColor();
 		}
-		BufferedImage monImage = getBrickImage(c);
+		
+		/* Colorize the brick image */
+		BufferedImage monImage = colorizeBrickImage(c);
 
 		if (ratio != 1) {
 			/*Resize image*/
@@ -61,6 +103,7 @@ public class Brick2D {
 
 		g.drawImage(monImage, top, left, null);
 
+		/* Draw shadow */
 		if (shadow) {
 			Graphics2D g2d = (Graphics2D) g;
 			Composite comp = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.5f);
@@ -72,13 +115,19 @@ public class Brick2D {
 			g2d.setComposite(comp);
 
 		} else {
+			/* Draw text */
 			g.setColor(Color.black);
 			g.setFont(new Font("Arial", Font.BOLD, (int) (ratio * 20)));
 			g.drawString(toUpperCase, top + (int) (ratio * 10), left + (int) (ratio * 25));
 		}
 	}
-
-	public BufferedImage getBrickImage(Color myColor) {
+	
+	/**
+	 * Colorize the brick image
+	 * @param myColor Color to colorize the brick image
+	 * @return Color to colorize the brick image
+	 */
+	private BufferedImage colorizeBrickImage(Color myColor) {
 		try {
 			BufferedImage monImage = ImageIO.read(new File(brickImg));
 			WritableRaster trame = monImage.getRaster();
@@ -89,36 +138,6 @@ public class Brick2D {
 					/*Get alpha of the image*/
 					Object pixel = trame.getDataElements(i, j, null);
 					int alpha = color.getAlpha(pixel);
-					/*Create new color with alpha of the image*/
-					myColor = new Color(myColor.getRed(), myColor.getGreen(), myColor.getBlue(), alpha);
-					int rgb = myColor.getRGB();
-					Object couleur = color.getDataElements(rgb, null);
-					trame.setDataElements(i, j, couleur);
-				}
-			}
-			return monImage;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	public BufferedImage getBrickShadow(Color myColor) {
-		try {
-			BufferedImage monImage = ImageIO.read(new File(brickImg));
-			WritableRaster trame = monImage.getRaster();
-			ColorModel color = monImage.getColorModel();
-
-			for (int i = 0; i < 35; ++i) {
-				for (int j = 0; j < 35; ++j) {
-					/*Get alpha of the image*/
-					Object pixel = trame.getDataElements(i, j, null);
-					int alpha = color.getAlpha(pixel);
-					if (alpha >= 110) {
-						alpha -= 110;
-					} else {
-						alpha = 0;
-					}
 					/*Create new color with alpha of the image*/
 					myColor = new Color(myColor.getRed(), myColor.getGreen(), myColor.getBlue(), alpha);
 					int rgb = myColor.getRGB();
